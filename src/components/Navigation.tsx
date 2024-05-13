0e2af7e4e563b32aeca78aed6d1bf9233e18bb0e
Petr Hendrych <petr.hendrych97@seznam.cz>
2024-05-13 10:30:55 +0200

    Adding language switch

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,31 +1,46 @@
-import { Nav } from "react-bootstrap";
+import { Button, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import CZ from "@/assets/images/cz.svg?react";
+import GB from "@/assets/images/gb.svg?react";
 
 const Navigation = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const handleSwitchLanguage = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "cs" : "en");
+  };
 
   return (
-    <Nav defaultActiveKey="/">
-      <Nav.Item>
-        <Nav.Link href="/">{t("navbar.home")}</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/cooking">
-          {t("navbar.cooking")}
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/IT">
-          {t("navbar.IT")}
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/gardening">
-          {t("navbar.gardening")}
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <div className="d-flex justify-content-between align-items-center">
+      <Nav defaultActiveKey="/">
+        <Nav.Item>
+          <Nav.Link href="/">{t("navbar.home")}</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/cooking">
+            {t("navbar.cooking")}
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/IT">
+            {t("navbar.IT")}
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/gardening">
+            {t("navbar.gardening")}
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Button variant="link" onClick={handleSwitchLanguage}>
+        {i18n.language === "en" ? (
+          <CZ height={25} width={25} />
+        ) : (
+          <GB height={25} width={25} />
+        )}
+      </Button>
+    </div>
   );
 };
 
